7c9ccd95d37b3d7f881b453a9a0b6fc0cd0cb481
kujoh <43499458+kujoh@users.noreply.github.com>
2023-04-15 16:20:54 -0400

    Update app.py

--- app.py
+++ app.py
@@ -7,3 +7,26 @@ from urllib.request import Request, urlopen
 from flask import Flask, request
 
 app = Flask(__name__)
+{
+  "attachments": [],
+  "avatar_url": "http://i.groupme.com/123456789",
+  "created_at": 1302623328,
+  "group_id": "1234567890",
+  "id": "1234567890",
+  "name": "John",
+  "sender_id": "12345",
+  "sender_type": "user",
+  "source_guid": "GUID",
+  "system": False,
+  "text": "Hello world ☃☃",
+  "user_id": "1234567890"
+}
+def send_message(msg):
+  url  = 'https://api.groupme.com/v3/bots/post'
+
+  data = {
+          'bot_id' : os.getenv('GROUPME_BOT_ID'),
+          'text'   : msg,
+         }
+  request = Request(url, urlencode(data).encode())
+  json = urlopen(request).read().decode()
